d9515dafa4c47f4699e7ab5b4a4527c81738ba0f
Miles Wallio <mwallio@gmail.com>
2010-01-27 16:23:11 -0500

    Added some objects

---
+++ objects/object.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+# Author: Miles Wallio, walliom@my.fit.edu
+# Course: CSE 4280, Section E1, Spring 2010
+# Project: Ray Tracing
+
+# Generic Shape Package... Just an interface type package...
+Package Object;
+
+# Default Constructor
+sub new {
+	my $class = shift;
+	my $self = {
+		_x	=>	shift,
+		_y	=>	shift,
+		_z	=>	shift,
+		_r	=>	shift,
+		_g	=>	shift,
+		_b	=>	shift,
+		_a	=>	shift,
+		_e	=>	shift,
+		_reflection	=>	shift,
+	};
+	
+	# Print output to consle
+	print "Created new " . $class
+	
+	bless $self, $class;
+	return $self;
+}
+
+sub intersects {
+	
+}
+1;
